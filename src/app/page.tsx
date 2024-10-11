4b47cf61b0b778e9200d6369a92c2dfed9191d87
DiegoDom <dominguezdiex@gmail.com>
2024-10-11 09:22:09 -0600

    redirect homepage to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-export default function Home() {
-  return (
-    <>
-      <span className="text-5xl">Hello World</span>
-    </>
-  );
+import { redirect } from "next/navigation";
+
+export default function HomePage() {
+  redirect("/dashboard");
 }
